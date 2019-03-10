9f79d4599737f8c401fa4827864daee9a6df57c6
Bishal Shrestha <bishalshrestha33@gmail.com>
2019-03-10 21:43:08 +1100

    Unzip tmp files in the file dir instead of gem dir

--- lib/de_dup/images.rb
+++ lib/de_dup/images.rb
@@ -7,15 +7,22 @@ module DeDup
   # Methods for image file types.
   module Images
     def self.process(zip_file)
-      DeDup::Utils.unzip(zip_file)
-      FileUtils.chdir('./tmp')
+      initial_path = Dir.pwd
+      file_dir = File.dirname(zip_file)
+      basename = File.basename(zip_file)
+
+      FileUtils.mkdir_p("#{file_dir}/#{basename}_tmp")
+      FileUtils.chdir(file_dir)
+      DeDup::Utils.unzip(basename)
+      FileUtils.chdir("./#{basename}_tmp")
       DeDup::Utils.print_results(
         DeDup::Utils.md5_map(
           DeDup::Utils.img_entries
         )
       )
       Dir.chdir('../')
-      FileUtils.rm_rf('tmp/.')
+      FileUtils.rm_rf("./#{basename}_tmp")
+      Dir.chdir(initial_path)
       'Done!'
     end
   end
