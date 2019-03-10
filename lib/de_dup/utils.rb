93d3d762de1dd39b55131b668b0650b121026bf1
Bishal Shrestha <bishalshrestha33@gmail.com>
2019-03-10 19:04:37 +1100

    Add method to return key value hash
    Returns a hash map with generated MD5 hash of a file as key and
    an array of the image file path along with it's duplicates if present as value.
    
    ;ex hash map:
    { 'a39aeb2e62c5f4ffd9a485913fc15b20' => ['file/path/img1.jpg', 'file/path/img2.jpg'],
      'dsn23b2e62c5f4ffd9a485913fc15b20' => ['file/path/to/another/img.jpg'] }

--- lib/de_dup/utils.rb
+++ lib/de_dup/utils.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'digest/md5'
+
 module DeDup
   # Common utility methods useful for different file types.
   module Utils
@@ -14,6 +16,19 @@ module DeDup
       Dir.glob('**/*').select do |entry|
         %w[.jpg .JPG .jpeg .png].include?(File.extname(entry))
       end
+    end
+
+    # Returns a hash map with generated MD5 hash of a file as key and
+    # an array of the image file path along with it's duplicates if present as value.
+    # { 'a39aeb2e62c5f4ffd9a485913fc15b20' => ['file/path/img1.jpg', 'file/path/img2.jpg'],
+    #   'dsn23b2e62c5f4ffd9a485913fc15b20' => ['file/path/to/another/img.jpg'] }
+    def self.md5_map(entries)
+      md5_map = {}
+      entries.each do |entry|
+        code = Digest::MD5.file(entry).to_s
+        md5_map[code] = (md5_map[code] || []) << entry
+      end
+      md5_map
     end
   end
 end
--- spec/de_dup/utils_spec.rb
+++ spec/de_dup/utils_spec.rb
@@ -33,11 +33,22 @@ RSpec.describe DeDup::Utils do
       ].sort
       Dir.chdir('spec/fixtures/expected_extract')
       expect(DeDup::Utils.img_entries.sort).to eq expected_entries
+      Dir.chdir('../../../')
     end
   end
 
   describe '.md5_map' do
-    it 'returns a map { md5_hash => [duplicate_file_paths] } given [file_paths]'
+    it 'returns a map { md5_hash => [duplicate_file_paths] } given [file_paths]' do
+      expected_result = {
+        'a39aeb2e62c5f4ffd9a485913fc15b20' => ['test_images/test_images/folder_1/The night sky.jpg', 'test_images/test_images/folder_2/The night sky_2.jpg'],
+        '472844f6f2a10019fdc18c9f4158f37b' => ['test_images/test_images/folder_1/staring contest.jpg', 'test_images/test_images/folder_2/staring contest_2.jpg', 'test_images/test_images/folder_3/staring contest_3.jpg'],
+        '0a0c4a48c521fa72864977d1677ecaee' => ['test_images/test_images/folder_1/sweet boat.jpg', 'test_images/test_images/folder_2/sweet boat_2.jpg'],
+        'a5751acf7c7af17ceae4baa615dfc14d' => ['test_images/test_images/folder_3/water taxis.jpg']
+      }
+      Dir.chdir('spec/fixtures/expected_extract')
+      img_entries = DeDup::Utils.img_entries.sort
+      expect(DeDup::Utils.md5_map(img_entries)).to eq(expected_result)
+    end
   end
 
   describe '.print_results' do
